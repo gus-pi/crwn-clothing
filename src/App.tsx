59340e201ef8106479ce64d5a5427a0ddac18efb
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-12-04 10:05:16 -0400

    add directory component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/category-item/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -30,18 +29,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem
-          key={category.id}
-          id={category.id}
-          imageUrl={category.imageUrl}
-          title={category.title}
-        />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
---
+++ src/components/category-item/directory/directory.component.tsx
@@ -0,0 +1,25 @@
+import CategoryItem from '../category-item.component';
+import './directory.styles.scss';
+
+type Category = {
+  id: number;
+  title: string;
+  imageUrl: string;
+};
+
+const Directory = ({ categories }: { categories: Category[] }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem
+          key={category.id}
+          id={category.id}
+          imageUrl={category.imageUrl}
+          title={category.title}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
