df2d3087d75b95b4fb6a853ab6815c2710edce6a
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-12-13 15:55:36 -0400

    add $ sign to product card

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -16,7 +16,7 @@ const ProductCard = ({ product }: { product: Product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
       <Button buttonType="inverted" onClick={addProductToCart}>
         Add to cart
