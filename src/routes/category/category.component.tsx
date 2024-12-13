f1c6d8af75fc80e48f796736d342ee283ed9563a
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-12-13 15:45:20 -0400

    add category page

---
+++ src/routes/category/category.component.tsx
@@ -0,0 +1,29 @@
+import { useParams } from 'react-router-dom';
+import './category.styles.scss';
+import { useContext, useEffect, useState } from 'react';
+import { CategoriesContext } from '../../context/categories.context';
+import { Product } from '../../utils/types/types';
+import ProductCard from '../../components/product-card/product-card.component';
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (category) {
+      setProducts(categoriesMap[category]);
+    }
+  }, [category, categoriesMap]);
+
+  return (
+    <div className="category-container">
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
+};
+
+export default Category;
